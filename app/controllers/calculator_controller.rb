74623be009040e40d3009ecceab8f8cdeefeef7f
joshuacassidy <joshcassidy79@gmail.com>
2022-02-14 20:48:19 +0000

    solution

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,25 +1,98 @@
 require 'arithmetic_calculator'
 
-class CalculatorController < ApplicationController
-  protect_from_forgery with: :exception, unless: -> {request.format.json?}
+class String
+  def is_numeric? 
+    true if Float(self) rescue false
+  end
+end
 
+class CalculatorController < ApplicationController
+  
   def sum
 
+    @title = "Sum"
+    @notice = ""
     # the user data is sent via the params
     # :number1 key contains the first number entered # :number2 key contains the second number entered # the numbers are entered as a text
     # retrieve the number submitted by the user
     @num1 = params[:number1]
     @num2 = params[:number2]
-    puts(params)
-    # we use the arithmetic_calculator.rb of the lib folder
-    @result = ArithmeticCalculator.add(@num1.to_i, @num2.to_i)
-
-    respond_to do |format|  ## Add this
-      format.json { render json: { "result" => @result }.to_json, status: :ok}
-      format.html 
-    end 
+    if(@num1 && @num2)
+      # we use the arithmetic_calculator.rb of the lib folder
+      if((@num1.is_numeric?) && (@num2.is_numeric?))
+        @result = ArithmeticCalculator.add(@num1.to_i, @num2.to_i)
+      else 
+        @notice = "You must supply numbers for both fields"
+      end
+    end
 
   end
 
+  def multiply
 
+    @title = "Multiply"
+    @notice = ""
+    # the user data is sent via the params
+    # :number1 key contains the first number entered # :number2 key contains the second number entered # the numbers are entered as a text
+    # retrieve the number submitted by the user
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    if(@num1 && @num2)
+      # we use the arithmetic_calculator.rb of the lib folder
+      if((@num1.is_numeric?) && (@num2.is_numeric?))
+        @result = ArithmeticCalculator.multiply(@num1.to_i, @num2.to_i)
+      else 
+        @notice = "You must supply numbers for both fields"
+      end
+    end
+
+  end
+
+  def subtract
+    
+    @title = "Subtract"
+    @notice = ""
+    # the user data is sent via the params
+    # :number1 key contains the first number entered # :number2 key contains the second number entered # the numbers are entered as a text
+    # retrieve the number submitted by the user
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    if(@num1 && @num2)
+      # we use the arithmetic_calculator.rb of the lib folder
+      if((@num1.is_numeric?) && (@num2.is_numeric?))
+        @result = ArithmeticCalculator.subtract(@num1.to_i, @num2.to_i)
+      else 
+        @notice = "You must supply numbers for both fields"
+      end
+    end
+
+  end
+
+  def divide
+
+    @title = "Divide"
+    @notice = ""
+    # the user data is sent via the params
+    # :number1 key contains the first number entered # :number2 key contains the second number entered # the numbers are entered as a text
+    # retrieve the number submitted by the user
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    if(@num1 && @num2)
+      # we use the arithmetic_calculator.rb of the lib folder
+      if((@num1.is_numeric?) && (@num2.is_numeric?))
+        # exception handling if user divides by zero
+        begin
+          @result = ArithmeticCalculator.divide(@num1.to_i, @num2.to_i)
+        rescue
+          @result = "Inf"
+        end
+        
+      else 
+        @notice = "You must supply numbers for both fields"
+      end
+    end
+
+
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root :controller=>'calculator', :action=>'calculate'
   get '/calculate', :controller=>'calculator', :action=>'calculate'
+  
   post '/sum', :controller=>'calculator', :action=>'sum'
+  get '/sum', :controller=>'calculator', :action=>'sum'
+  
+  post '/subtract', :controller=>'calculator', :action=>'subtract'
+  get '/subtract', :controller=>'calculator', :action=>'subtract'
+  
+  post '/divide', :controller=>'calculator', :action=>'divide'
+  get '/divide', :controller=>'calculator', :action=>'divide'
+  
+  post '/multiply', :controller=>'calculator', :action=>'multiply'
+  get '/multiply', :controller=>'calculator', :action=>'multiply'
 end
